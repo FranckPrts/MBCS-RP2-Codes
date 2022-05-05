a23ca82204729d1c1e522cc4bb4b0d4582ce1188
Franck Porteous <franck.porteous@protonmail.com>
2022-05-05 09:19:21 +0200

    added py script to find dropped epochs in EEG

---
+++ find_dropped_epochs.py
@@ -0,0 +1,27 @@
+#%%
+from utils import eeg_sw
+import os
+
+path = "../YOUR-PATH/SNS_ES_cleaned/"
+# The path must be changed for each folder containning SET files
+
+l_files = os.listdir(path)
+
+dd = dict()
+#%%
+for i in l_files:
+    if i[0] == "." or i[-3:] != "set":
+        pass
+    else:
+        print(path+i)
+        sub = eeg_sw.import_set_custom(path+i, sw_min=None, sw_max=None)
+        dd[i] = sub.df["epoch_id"].to_numpy()
+
+#  The dict "dd" now contains for each key (filename with the appendix .set) 
+#  a value being the numpy array with the epochs that weren't dropped.
+# %%
+len(dd)
+
+#%%
+# To access the retainned epochs of a subject, you may use:
+dd["THE-FILE-NAME.set"]
